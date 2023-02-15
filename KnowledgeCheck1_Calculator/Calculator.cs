d9952702acb67ecc54652b1cf4fd2b30730c9e14
Jody VanHoose <jodyvanhoose80@gmail.com>
2023-02-14 20:37:21 -0500

    first commit

---
+++ KnowledgeCheck1_Calculator/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck1_Calculator
+{
+    public class Calculator
+    {
+        public int Add(int first, int second)
+        {
+            return first + second;
+        }
+
+        public int Subtract(int first, int second)
+        {
+            return first - second;
+        }
+
+        public int Multiply(int first, int second)
+        {
+            return first * second;
+        }
+
+        public double Divide(double first, double second)
+        {
+            return first / second;
+        }
+
+
+        // Error msg if an invalid input(not a number)
+        public void NotInt()
+        {
+            Console.WriteLine("One or more of the numbers is not an int");
+        }
+
+
+        // Accept user input for 2 numbers
+        public (string, string) InputNumbers(string calc)
+        {
+            Console.WriteLine($"Enter 2 integers to {calc}");
+            var number1 = Console.ReadLine();
+            var number2 = Console.ReadLine();
+
+            return (number1, number2);
+        }
+    }
+}
---
+++ KnowledgeCheck1_Calculator/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck1_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello. Press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division");
+
+            var input = Console.ReadLine();
+            var calculator = new Calculator();
+
+            switch (input)
+            {
+                case "1":
+                    // Get user input for two numbers
+                    (var number1, var number2) = calculator.InputNumbers("add");
+
+                    // If valid numbers do addition calculation
+                    if (int.TryParse(number1, out int numOne) && int.TryParse(number2, out int numTwo))
+                    {
+                        Console.Write($"{number1} + {number2} = ");
+                        Console.Write(calculator.Add(numOne, numTwo));
+                    }
+                    else
+                    {
+                        // Error msg if not a valid number
+                        calculator.NotInt();
+                    }
+                    break;
+
+                case "2":
+                    // Get user input for two numbers
+                    (number1, number2) = calculator.InputNumbers("subtract");
+
+                    // If valid numbers do subtraction calculation
+                    if (int.TryParse(number1, out numOne) && int.TryParse(number2, out numTwo))
+                    {
+                        Console.Write($"{number1} - {number2} = ");
+                        Console.Write(calculator.Subtract(numOne, numTwo));
+                    }
+                    else
+                    {
+                        // Error msg if not a valid number
+                        calculator.NotInt();
+                    }
+                    break;
+
+                case "3":
+                    // Get user input for two numbers
+                    (number1, number2) = calculator.InputNumbers("multiply");
+
+                    // If valid numbers do muliplication calculation
+                    if (int.TryParse(number1, out int timesNumOne) && int.TryParse(number2, out int timesNumTwo))
+                    {
+                        Console.Write($"{number1} * {number2} = ");
+                        Console.Write(calculator.Multiply(timesNumOne, timesNumTwo));
+                    }
+                    else
+                    {
+                        // Error msg if not a valid number
+                        calculator.NotInt();
+                    }
+                    break;
+
+                case "4":
+                    // Get user input for two numbers
+                    (number1, number2) = calculator.InputNumbers("divide");
+
+                    // If valid numbers do division calculation
+                    if (double.TryParse(number1, out double divNumOne) && double.TryParse(number2, out double divNumTwo))
+                    {
+                        Console.Write($"{number1} / {number2} = ");
+                        Console.Write(calculator.Divide(divNumOne, divNumTwo));
+                    }
+                    else
+                    {
+                        // Error msg if not a valid number
+                        calculator.NotInt();
+                    }
+                    break;
+                    
+                    // Error msg if not 1, 2, 3, or 4 input
+                default:
+                    Console.WriteLine("Unknown input");
+                    break;
+            }
+        }
+    }
+}
